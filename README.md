08c3f95481e86f9e346ea59f83852fd992a3191b
Josh Grube <steamjgrube@gmail.com>
2021-05-08 17:14:24 -0400

    saving work

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # random-password-generator
+
+## Created functionality on a site to randomly generate a password based on 4 critera.
+
+## User gets to choose the desired size of their password
+
+## User is able to choose if they would like Uppercase & Lowercase characters, as well as numbers and symbols.
+
+## If the user chooses no critera, the prompt closes.
+
+
+
+### had some formatting issues causing my script to break, but was able to resolve to problem. Some feedback on my layout/format would be appreciated.
---
+++ assets/js/script.js
@@ -0,0 +1,110 @@
+// Assignment Code
+var user;
+var userNum;
+var userSym;
+var userUpper;
+var userLower;
+var choices;
+var generateBtn = document.querySelector("#generateBtn"); 
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-"];
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+var toUpper = function (u) {
+  return u.toUpperCase(); // for uppercase conversion
+};
+
+convertUpper = alphabet.map(toUpper);
+
+generateBtn.addEventListener("click", function() {
+  pass = getPassword();
+  document.getElementById("password").placeholder = pass;
+});
+
+function getPassword() {
+    user = parseInt(prompt("How long would you like your password to be? (8-128 characters)"));
+    if (!user) {
+      return;
+    } 
+    else {
+        userNum = confirm("Will your password contain numbers?");
+        userSym = confirm("Will your password contain symbols?");
+        userUpper = confirm("Will your password contain Uppercase letters?");
+        userLower = confirm("Will your password contain Lowercase letters?");
+    };
+
+    if (!userNum && !userSym && !userLower && !userUpper) {  //If cancel is selected 4 times.
+      choices = alert("Your password must include at least 1 criteria.");
+    
+    } 
+    else if (userNum && userSym && userLower && userUpper) { //If OK is selected 4 times.
+        choices = numbers.concat(symbols, convertUpper, alphabet);
+    };
+    // If the user chooses 1 option.
+    if (userNum) {
+      choices = numbers;
+    } 
+    else if (userSym) {
+      choices = symbols;
+    } 
+    else if (userLower) {
+      choices = alphabet;
+    } 
+    else if (userUpper) {
+      choices = space.concat(convertUpper)
+    };
+      // If the user chooses 2 options.
+    if (userSym && userNum) {
+      choices = symbols.concat(numbers);
+
+    } 
+    else if (userSym && userLower) {
+      choices = symbols.concat(alphabet);
+
+    } 
+    else if (userSym && userUpper) {
+      choices = symbols.concat(convertUpper);
+
+    } 
+    else if (userLower && userNum) {
+      choices = alphabet.concat(numbers);
+
+    } 
+    else if (userLower && userUpper) {
+      choices = alphabet.concat(convertUpper);
+
+    } 
+    else if (userNum && userUpper) {
+      choices = numbers.concat(convertUpper);
+    }
+    // If the user chooses 3 options.
+    if (userSym && userNum && userUpper) {
+      choices = symbols.concat(numbers, convertUpper);
+
+    } 
+    else if (userSym && userNum && userLower) {
+      choices = symbols.concat(numbers, alphabet);
+
+    } 
+    else if (userSym && userLower && userUpper) {
+      choices = symbols.concat(alphabet, convertUpper);
+
+    } 
+    else if (userLower && userNum && userUpper) {
+      choices = numbers.concat(convertUpper, alphabet);
+    };
+    var password = [];
+
+    for (var i = 0; i < user; i++) {
+      var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+      password.push(pickChoices);
+    }
+    var pass = password.join("");
+    UserInput(pass);
+    return pass;
+  }
+
+function UserInput(pass) {
+  document.getElementById("password").textContent =  pass;
+}
